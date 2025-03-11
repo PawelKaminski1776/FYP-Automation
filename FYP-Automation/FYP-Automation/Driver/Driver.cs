fa2e0898660ad4bc2e145f16f4f5e6958e38d76a
pawelkaminski1776 <pawelkaminski1776@gmail.com>
2025-03-11 19:13:56 +0000

    Initial Automation Webdriver file created

---
+++ FYP-Automation/FYP-Automation/Driver/Driver.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System.Runtime.InteropServices;
+
+namespace FYPAutomation.Driver
+{
+    public class Driver
+    {
+        private WebDriver driver;
+        private int RetryCount = 3;
+        public Driver(WebDriver Driver)
+        {
+            this.driver = Driver;
+        }
+
+        public void GoToWebsite(string url)
+        {
+            driver.Navigate().GoToUrl(url);
+        }
+
+        public void RetryableElementClick(By Element)
+        {
+            int count = 0;
+            while (RetryCount < count)
+            {
+                try
+                {
+                    driver.FindElement(Element).Click();
+                }
+                catch (Exception e)
+                {
+                    count++;
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        public void RetryableElementInsert(By Element, string text)
+        {
+            int count = 0;
+            while (RetryCount < count)
+            {
+                try
+                {
+                    driver.FindElement(Element).SendKeys(text);
+                }
+                catch (Exception e)
+                {
+                    count++;
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+
+        public void Quit()
+        {
+            driver.Quit();
+        }
+    }
+}
---
+++ FYP-Automation/FYP-Automation/End-To-End-Tests/FullApplicationTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FYPAutomation.End_To_End_Tests
+{
+    class FullApplicationTest
+    {
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
